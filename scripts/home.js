51d6ec62f367e08e0d13a349f402775716bc6295
LisaOluchi <sangqulisa@gmail.com>
2024-09-26 15:57:27 +0200

    css2

---
+++ scripts/home.js
@@ -0,0 +1,8 @@
+<script>
+    const hamburger = document.querySelector('.hamburger-menu');
+    const navMenu = document.querySelector('.nav-menu');
+
+    hamburger.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+    });
+</script>
